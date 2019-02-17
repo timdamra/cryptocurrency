cb216f1db07a9ab48f18c6337b2b85fd618d19c2
Hatim Damra <hatim.damra@redbox.com>
2019-02-16 22:02:57 -0600

    firebase config

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+const functions = require('firebase-functions');
+
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello irebase!");
+// });
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hey')
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+    render = () => {
+        return (
+            <p>Hello World</p>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
