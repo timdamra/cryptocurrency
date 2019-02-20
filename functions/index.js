acfd5962b26b35968b1dc59927c339599b6fc900
Hatim Damra <hatim.damra@redbox.com>
2019-02-19 19:53:44 -0600

    integrated firebase functions with express back-end

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
-const app = require('../app')
 const functions = require('firebase-functions')
+const express = require('express')
+const app = require('../app')
+
+console.log(app)
 
 exports.mainApp = functions.https.onRequest(app)
